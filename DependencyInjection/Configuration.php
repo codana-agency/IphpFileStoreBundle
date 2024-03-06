39f5faaec48656568c134adfb4e0d7ffa5f75861
Ward Vandevoort <ward.vandevoort@calibrate.be>
2024-03-06 16:04:48 +0100

    [code] Removed whitespace.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -15,14 +15,13 @@ class Configuration implements ConfigurationInterface
     /**
      * {@inheritDoc}
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $tb = new TreeBuilder('iphp_file_store');
 
         $tb
             ->children()
                 ->scalarNode('db_driver')->defaultValue('orm')->end()
-
                 ->arrayNode('mappings')
                     ->useAttributeAsKey('id')
                     ->prototype('array')
@@ -31,17 +30,13 @@ class Configuration implements ConfigurationInterface
                             ->scalarNode('upload_path')->end()
                             ->scalarNode('delete_on_remove')->defaultTrue()->end()
                             ->scalarNode('overwrite_duplicates')->defaultFalse()->end()
-
-
                             ->arrayNode('namer')
                                ->treatFalseLike(array ())
                                ->treatNullLike(array ('translit' => array('service' => 'iphp.filestore.namer.default')))
                                ->treatTrueLike(array ('translit' => array('service' => 'iphp.filestore.namer.default')))
                                ->useAttributeAsKey('id')
                                ->prototype('array')
-
                                   ->children()
-
                                      ->scalarNode('service')->defaultValue('iphp.filestore.namer.default')->end()
                                      ->arrayNode('params')
                                         ->useAttributeAsKey('name')
@@ -50,26 +45,18 @@ class Configuration implements ConfigurationInterface
                                  ->end()
                                ->end()
                             ->end()
-
-
-
                            ->arrayNode('directory_namer')
-
                              ->useAttributeAsKey('id')
                              ->prototype('array')
-
                                ->children()
-
                                   ->scalarNode('service')->defaultValue('iphp.filestore.directory_namer.default')->end()
                                   ->arrayNode('params')
                                     ->useAttributeAsKey('name')
                                     ->prototype('scalar')->end()
                                   ->end()
-
                                ->end()
                              ->end()
                             ->end()
-
                         ->end()
                     ->end()
                 ->end()
